5cb73693aacfacc47330b475278350a652ac915f
Jerry Martins <zeroumbin@gmail.com>
2018-11-03 09:00:19 -0400

    ajustes entidades e testes

--- src/main/java/com/kazale/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/com/kazale/pontointeligente/api/repositories/LancamentoRepository.java
@@ -7,19 +7,19 @@ import javax.persistence.NamedQuery;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kazale.pontointeligente.api.entities.Lancamento;
 
-@Transactional(readOnly=true)
+@Transactional(readOnly = true)
 @NamedQueries({
-	@NamedQuery(name="LancamentoRepository.findByFuncionarioId",
-			query="SELECT lanc FROM lancamento WHERE lanc.funcioanrio.id= :funcionarioId")
-})
-public interface LancamentoRepository {
-	
+		@NamedQuery(name = "LancamentoRepository.findByFuncionarioId", 
+				query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
 	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
-	
+
 	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
 }
